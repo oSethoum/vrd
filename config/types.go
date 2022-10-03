2c6c6d2240edb45db9ab53906645fa5be5aefd56
Oussama Sethoum <os.sethoum@gmail.com>
2022-10-03 09:30:44 +0100

    move: PrivacyNode to be internal

--- config/types.go
+++ config/types.go
@@ -12,7 +12,7 @@ type Ent struct {
 	Graphql     bool   `json:"graphql"`
 	Auth        bool   `json:"auth"`
 	Privacy     bool   `json:"privacy"`
-	PrivacyNode bool   `json:"privacyNode"`
+	PrivacyNode bool   `json:"-"`
 	FileUpload  bool   `json:"fileUpload"`
 	Debug       bool   `json:"debug"`
 	Database    string `json:"database"` //sqlite3, mysql, postgres
